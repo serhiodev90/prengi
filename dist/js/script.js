49c318c929501cc718393d7a643fd39d93ecd5a1
serhiodev90 <lokom286@yandex.ru>
2021-01-03 01:39:01 +0300

    Adaptive offer (bp768px)

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
+	$('.offer__burger').click(function(event) {
+		$('.offer__burger, .offer__menu').toggleClass('active');
+	});
+
 	$('.carousel').slick({
 		dots: true,
 		infinite: true,
 		speed: 500,
 		fade: true,
-		cssEase: 'linear'
+		cssEase: 'linear',
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+					dots: false
+				}
+			}
+		]
 	});
  });
