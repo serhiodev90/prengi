9f8cd070d0120ec4cbd3191815e7285b28394dc4
serhiodev90 <lokom286@yandex.ru>
2021-01-31 19:28:57 +0300

    First Modal + Adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
+
+	//Burger
+
 	$('.offer__burger').click(function(event) {
 		$('.offer__burger, .offer__menu').toggleClass('active');
 	});
+
+	//Slider
 
 	$('.carousel').slick({
 		dots: true,
@@ -9,6 +14,15 @@ $(document).ready(function(){
 		speed: 500,
 		fade: true,
 		cssEase: 'linear',
+	});
+
+	//Modal
+
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function() {
+		$('.overlay, #consultation, #thanks #contact').fadeOut('slow');
 	});
 
 	new WOW().init();
